3fcf6271f16f1b12d4aeabdf8d6a0c0ae45338b7
Ipatie Daniel <gruesc.ipatie@gmail.com>
2021-03-27 19:08:15 +0200

    Heroky try 1

---
+++ frontend/src/components/homePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="mainContainer">
+    
+    <div id="upperHalf">
+    </div>
+
+    <div id="lowerHalf">
+      <div class="cardText">
+        <h1> LA Beach </h1>
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
+
+<style>
+<++>
+
+</style>
+<script>
+<++>
+
+</script>
